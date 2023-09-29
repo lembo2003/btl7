dd22e226062e6c71c63d55826bad03abbdb10251
Lembo <54678184+lembo2003@users.noreply.github.com>
2023-09-30 04:05:51 +0700

    Add files via upload

---
+++ BUS/BUS_admincontrol.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+namespace BUS
+{
+    public class BUS_admincontrol
+    {
+        DAL_admincontrol admin = new DAL_admincontrol();
+        public DataTable admin_LoadDGV()
+        {
+            return admin.admin_LoadDGV();
+        }
+        public static DTO_admincontrol GetUserInformationAdminC(string username)
+        {
+            DAL_admincontrol dal = new DAL_admincontrol();
+            return dal.GetUserInformationAdminC(username);
+        }
+    }
+}
---
+++ BUS/BUS_info.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+namespace BUS
+{
+    public class BUS_info
+    {
+        DAL_info dal = new DAL_info();
+        public static DTO_info GetUserInformation(string username)
+        {
+            DAL_info daL = new DAL_info();
+            return daL.GetUserInformation(username);
+        }
+        public void Update_info(DTO_info ob)
+        {
+            dal.Update_info(ob);
+        }
+    }
+}
