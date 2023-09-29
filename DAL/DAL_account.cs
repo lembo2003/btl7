6cb8dd240ab95bff519056f78247ad6d2ca82287
Lembo <54678184+lembo2003@users.noreply.github.com>
2023-09-30 04:06:27 +0700

    Add files via upload

---
+++ DAL/DAL_account.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DAL_account : DAL_connection
+    {
+        public static bool Check_login(DTO_account ob)
+        {
+            DAL_connection dal = new DAL_connection();
+            string sql = "select * from login where username='" + ob.tk + "' and password='" + ob.mk + "'";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@tk", ob.tk);
+            cmd.Parameters.AddWithValue("@mk", ob.mk);
+            DataTable dt = dal.Load(sql);
+            return dt.Rows.Count > 0;
+        }
+        public static string Check_type(DTO_account ob)
+        {
+            string account_type = null;
+            DAL_connection dal = new DAL_connection();
+            string sql = "SELECT type FROM login WHERE username ='"+ob.tk+"'";
+            
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@tk", ob.tk);
+
+            SqlDataReader read = dal.Read(sql);
+            if (read.Read())
+            {
+                if(!read.IsDBNull(0))
+                {
+                    account_type = read.GetString(0);
+                }
+            }
+            return account_type;
+
+        }
+        public DTO_account GetUserInformationLogin(string tk)
+        {
+            DAL_connection dal = new DAL_connection();
+            DTO_account account = new DTO_account();
+            string sql = "SELECT * FROM login WHERE username = @tk";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@tk", tk);
+            SqlDataReader reader = dal.Read(cmd);
+            while (reader.Read())
+            {
+                //account.tk = reader["tk"].ToString();
+                account.tk = reader["username"].ToString(); // Use the correct column name
+                account.mk = reader["password"].ToString();
+            }
+            return account;
+        }
+        public void Update_login(DTO_account ob)
+        {
+            string sql = "update login set username = '" + ob.tk + "',password = '" + ob.mk + "'where username = '" + ob.tk + "'";
+            Excecute(sql);
+        }
+    }
+}
---
+++ DAL/DAL_admincontrol.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data;
+using System.Data.SqlClient;
+namespace DAL
+{
+    public class DAL_admincontrol : DAL_connection
+    {
+        public DataTable admin_LoadDGV()
+        {
+            return Load("SELECT login.username AS username, login.password,login.type,account_info.hoten,account_info.diachi,account_info.email,account_info.sdt FROM login JOIN account_info ON login.username = account_info.username;");
+        }
+        public DTO_admincontrol GetUserInformationAdminC(string username)
+        {
+            DAL_connection dal = new DAL_connection();
+            DTO_admincontrol info = new DTO_admincontrol();
+            string sql = "SELECT * FROM account_info JOIN login ON account_info.username = login.username WHERE login.username = @username";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Parameters.AddWithValue("@username", username);
+            SqlDataReader reader = dal.Read(cmd);
+            while (reader.Read())
+            {
+                info.hoten = reader["hoten"].ToString();
+                info.email = reader["email"].ToString();
+                info.diachi = reader["diachi"].ToString();
+                info.sdt = reader["sdt"].ToString();
+                info.username = reader["username"].ToString();
+                info.password = reader["password"].ToString(); 
+                info.type = reader["type"].ToString();
+            }
+            return info;
+        }
+    }
+}
