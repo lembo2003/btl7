b23e5ea83591ac1569551048170eb28c74d85b6f
Lembo <54678184+lembo2003@users.noreply.github.com>
2023-09-30 21:53:25 +0700

    Add files via upload

---
+++ BUS/BUS_GD_noiquy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+using System.Data;
+namespace BUS
+{
+    public class BUS_GD_noiquy
+    {
+        DAL_GD_noiquy GD = new DAL_GD_noiquy();
+        public DataTable Load_noiquy_nonGD()
+        {
+            return GD.Load_noiquy_nonGD();
+        }
+    }
+}
